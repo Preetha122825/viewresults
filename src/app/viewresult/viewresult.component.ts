6f41a928945411afa4e2750dbb3d919dd1d7df88
Preetha122825 <preethacse25@gmail.com>
2025-04-11 11:26:35 +0530

    added

---
+++ src/app/viewresult/viewresult.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewresult',
+  imports: [],
+  templateUrl: './viewresult.component.html',
+  styleUrl: './viewresult.component.css'
+})
+export class ViewresultComponent {
+
+}
